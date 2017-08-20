6db76df189bc080a0fa4153522f05a0918c10338
Michal Krutul <michal.kutul@gmail.com>
2017-08-20 17:06:07 +0200

    First steps with application

---
+++ Pr/src/PlayerGenerator.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class PlayerGenerator {
+
+
+
+
+
+    public static void generatePlayer(Scanner scanner){
+        System.out.println("Give sex (1 - male, 2 - female): ");
+        String wybor = scanner.next();
+        System.out.println("");
+    }
+
+}
