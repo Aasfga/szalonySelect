8f7c9d277b5b9fbb0fc261eefd210684522a10a2
lwronski <woljako@gmail.com>
2017-08-30 18:04:31 +0200

    Add files via upload

--- Pr/src/common/generators/Manually.java
+++ Pr/src/common/generators/Manually.java
@@ -2,10 +2,14 @@ package common.generators;
 
 import common.Judge;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Objects;
 import java.util.Scanner;
+import java.util.concurrent.CountDownLatch;
+
+import static common.MainApp.displayResultSet;
 
 public class Manually {
     public static Manually.Builder builder(Statement statement) {
@@ -84,6 +88,61 @@ public class Manually {
                Communication.error( e );
             }
             System.out.println("Success! " + name+  "added.\n");
+        }
+
+        public void team(){
+            Communication.hello( "team" );
+            String sex=Communication.enter( "sex","1-male, 2-female" );
+
+            try {
+                Communication.hello( "Select discipline");
+                String sql = "SELECT d.id, c.name FROM disciplines as d left join categories as c on d.id_categories = c.id where id_sex = " + sex + ";";
+                ResultSet rs = statement.executeQuery(sql);
+                displayResultSet(rs);
+                String discipline =Communication.enter("Select ID" );
+
+                Communication.hello( "Select nationality");
+                sql = "SELECT * FROM nationalities;";
+                rs = statement.executeQuery(sql);
+//                displayResultSet(rs);
+                String nationalities =Communication.enter("Select ID" );
+
+                sql = "INSERT INTO teams VALUES(DEFAULT ," + sex + "," + discipline + "," + nationalities+ ");";
+                statement.execute(sql);
+                String teamID = preparer.lastUsedDefaultID("teams");
+
+
+                sql = "select * from categories where id = " + discipline +";";
+                rs = statement.executeQuery(sql);
+                rs.next();
+                int minplayer = rs.getInt(5);
+                int maxplayer = rs.getInt(6);
+                String numberplayers =Communication.enter("Select number of players in team (" + minplayer + "," + maxplayer + ")" );
+
+                sql = "SELECT * FROM players where id_sex = " + sex + " and id_nationality = " + nationalities + ";";
+                rs = statement.executeQuery(sql);
+
+                int counter = 0;
+                while( rs.next( )) counter++;
+                if ( counter < Integer.parseInt(numberplayers) ) {
+                    Communication.hello( "Not enough players, first add new players");
+                    displayResultSet(rs);
+                    return;
+                }
+
+                Communication.hello( "Select players");
+                displayResultSet(rs);
+
+                for ( int i = 0; i <  Integer.parseInt(numberplayers); i++ ) {
+                    String id_players =Communication.enter("Select ID" );
+                    sql = "INSERT INTO player_team VALUES("+ id_players + "," + teamID + ");";
+                    statement.execute(sql);
+                }
+
+            } catch (SQLException e) {
+                Communication.error( e );
+            }
+            System.out.println("Success!\n");
         }
     }
 
