4813aaeac572bc1ff8609abf94228ddd025c9ace
lwronski <woljako@gmail.com>
2017-08-30 21:06:47 +0200

    Improved team generation

--- Pr/src/common/Team.java
+++ Pr/src/common/Team.java
@@ -3,9 +3,11 @@ package common;
 import common.generators.Preparer;
 import common.generators.Randomise;
 
+import javax.xml.transform.Result;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.LinkedHashSet;
 import java.util.Random;
 
@@ -51,25 +53,13 @@ public class Team {
 
         public void add() throws SQLException {
             Random random = new Random();
-            int counter = 0;
-            String randomdiscipline;
+            Preparer preparer = new Preparer(statement);
+
             sexID = ( sexID == null ) ? randomise.randomIdFromTable("sexes") : sexID;
 
-            String sql = "SELECT count(*) from disciplines where id_sex = " + sexID + ";";
-            ResultSet rs = statement.executeQuery(sql);
-            rs.next();
-            int randomnumber = random.nextInt(rs.getInt(1));
-            sql = "SELECT id from disciplines  where id_sex = " + sexID + ";";
-            rs = statement.executeQuery(sql);
-            while(true){
-                rs.next();
-                if ( counter == randomnumber) {
-                    randomdiscipline = rs.getString(1);
-                    break;
-                }
-                counter++;
-            }
-
+            String sql = "CREATE OR REPLACE VIEW disciplineview AS SELECT id from disciplines where id_sex = " + sexID + ";";
+            statement.execute(sql);
+            String randomdiscipline = randomise.randomIdFromTable("disciplineview");
 
             disciplineID = ( disciplineID == null ) ?  randomdiscipline  : disciplineID;
             nationalityID = ( nationalityID == null ) ? randomise.randomIdFromTable("nationalities") : nationalityID;
@@ -78,58 +68,35 @@ public class Team {
             statement.execute(sql);
             System.out.println(sql);
 
-
-            sql = "SELECT c.min_players_team, c.max_players_team from disciplines as d join categories as c on c.id = d.id_categories where id_sex = " + sexID + " and d.id = " + disciplineID + ";";
-            rs = statement.executeQuery(sql);
+            sql = "SELECT * from disciplines as d join categories as c on c.id = d.id_categories where id_sex = " + sexID + " and d.id = " + disciplineID + ";";
+            ResultSet rs = statement.executeQuery(sql);
             rs.next();
-            int min_players_team = rs.getInt(1);
-            int max_players_team = rs.getInt(2);
-
+            int min_players_team = rs.getInt(8);
+            int max_players_team = rs.getInt(9);
+            System.out.println(min_players_team + " " + max_players_team);
             sql = "SELECT * from disciplines as d join categories as c on c.id = d.id_categories where id_sex = " + sexID + " and d.id = " + disciplineID + ";";
             rs = statement.executeQuery(sql);
             displayResultSet(rs);
 
+
             int randomnumber_players = random.nextInt(max_players_team + 1 - min_players_team) + min_players_team;
-
-            sql = " SELECT * from players where id_sex = " + sexID + " and id_nationality = " + nationalityID+ ";";
+            sql = " SELECT id from players where id_sex = " + sexID + " and id_nationality = " + nationalityID+ ";";
             rs = statement.executeQuery(sql);
-            counter = 0;
-            while (rs.next() ) counter++;
+            ArrayList players = preparer.arrayFromResultSetColumn(rs,1);
 
-
-            while( counter < randomnumber_players) {
+            while( players.size() < randomnumber_players) {
                 Player.builder(statement).withID(sexID).withNationalityID(nationalityID).add();
-                counter++;
+                players.add(preparer.lastUsedDefaultID("players"));
             }
 
-
-            LinkedHashSet list_random_int = new LinkedHashSet();
-            do{
-                int number = random.nextInt(counter);
-                list_random_int.add(number);
-            } while ( list_random_int.size() < randomnumber_players);
-
-
-            counter = 0;
             String teamID = preparer.lastUsedDefaultID("teams");
 
-            sql = " SELECT * from players where id_sex = " + sexID + " and id_nationality = " + nationalityID+ ";";
-            rs = statement.executeQuery(sql);
 
-
-            try {
-                   while( rs.next()) {
-                       if ( list_random_int.contains(counter)) {
-                           sql = "INSERT INTO player_team values ( " + rs.getString(1) + " , " + teamID + " );";
-                           statement.execute(sql);
-                           System.out.println(sql);
-                       }
-                       counter++;
-                   }
-               } catch (SQLException e) {
-//            TODO
-                  e.printStackTrace();
-               }
+            while( randomnumber_players-- > 0  ) {
+                sql = "INSERT INTO player_team values ( " + players.get(randomnumber_players) + " , " + teamID + " );";
+                    statement.execute(sql);
+                    System.out.println(sql);
+                }
 
         }
 
