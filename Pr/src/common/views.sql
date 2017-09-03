8f0d1f185d2090ae0317c082589af0b49acbe0d8
marcinpodhajski <podhajskimarcin@gmail.com>
2017-09-03 13:01:19 +0200

    Update views.sql

--- Pr/src/common/views.sql
+++ Pr/src/common/views.sql
@@ -18,10 +18,10 @@ CREATE VIEW team_category AS SELECT
 
 CREATE VIEW results AS SELECT
  country, category,id_final as final, finals.name, event_team_result.result as result
-from team_category
-join event_team_result on id_team=team
-join events on id_event=events.id
-join finals on id_final=finals.id;
+ from team_category
+ join event_team_result on id_team=team
+ join events on id_event=events.id
+ join finals on id_final=finals.id;
 
 CREATE VIEW players_all AS SELECT
       players.*,weights.weight,weighst.date
@@ -29,25 +29,25 @@ CREATE VIEW players_all AS SELECT
             join weights on weights.id_player=players.id;
 
 CREATE VIEW lol AS
-select
-final,category, max(result) as scoremax from results where final=1 group by category,final;
+ select
+ final,category, max(result) as scoremax from results where final=1 group by category,final;
 
 CREATE VIEW lol2 AS
-select
-final,category, min(result) as scoremax from results where final=1 group by category,final;
+ select
+ final,category, min(result) as scoremax from results where final=1 group by category,final;
 
 CREATE VIEW lol3 AS
-select
-final,category, max(result) as scoremax from results where final=2 group by category,final;
+ select
+ final,category, max(result) as scoremax from results where final=2 group by category,final;
 
 CREATE VIEW gold_medals AS SELECT country,lol.category
-from results join lol on results.final=lol.final
-       where results.result=lol.scoremax;
+ from results join lol on results.final=lol.final
+        where results.result=lol.scoremax;
 
 CREATE VIEW silver_medals AS SELECT country,lol2.category
-from results join lol2 on results.final=lol2.final
+ from results join lol2 on results.final=lol2.final
        where results.result=lol2.scoremax;
 
 CREATE VIEW bronze_medals AS SELECT country,lol3.category
-from results join lol3 on results.final=lol3.final
+ from results join lol3 on results.final=lol3.final
        where results.result=lol3.scoremax;
