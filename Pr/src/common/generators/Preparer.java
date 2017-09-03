a1f897144ef08d26e5dcfb4a3086382e6d08aafe
lwronski <woljako@gmail.com>
2017-09-03 21:57:12 +0200

    remove code redundancy

--- Pr/src/common/generators/Preparer.java
+++ Pr/src/common/generators/Preparer.java
@@ -1,6 +1,7 @@
 package common.generators;
 
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
@@ -44,4 +45,34 @@ public class Preparer {
 
         return arrayList.contains(id);
     }
+
+    public static void displayResultSet(ResultSet rs) {
+        try {
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int columnsNumber = rsmd.getColumnCount();
+
+            for (int i = 1; i <= columnsNumber; i++) {
+
+                int ile = 15 - rsmd.getColumnName(i).length();
+                String spaces = String.format("%" + ile + "s", "");
+                System.out.print(rsmd.getColumnName(i) + spaces);
+            }
+            System.out.println();
+
+            while (rs.next()) {
+
+                for (int i = 1; i <= columnsNumber; i++) {
+
+                    int ile = 15 - rs.getString(i).length();
+                    String spaces = String.format("%" + ile + "s", "");
+                    System.out.print(rs.getString(i) + spaces);
+                }
+                System.out.println();
+            }
+        } catch (SQLException e) {
+//            TODO
+//            e.printStackTrace();
+        }
+    }
+
 }
