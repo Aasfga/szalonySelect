84c98866e5f15e32db0e10337a200448047fd0c3
Marcin Podhajski <podhajskimarcin@gmail.com>
2017-09-03 16:06:26 +0200

    porozdzielalem manually do klas

---
+++ Pr/src/common/Category.java
@@ -0,0 +1,58 @@
+package common;
+
+import common.generators.Communication;
+import common.generators.Manually;
+import common.generators.Preparer;
+import common.generators.Randomise;
+
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Category {
+    public static Builder builder(Statement statement) {
+        return new Builder( statement );
+    }
+    private static Scanner scanner = new Scanner(System.in);
+    public static class Builder {
+        final private Preparer preparer;
+        private Statement statement;
+        public Builder(Statement statement) {
+            this.statement = statement;
+
+            this.preparer = new Preparer( statement );
+        }
+
+        public void manually() {
+            Communication.hello( "category" );
+            String name = Communication.enter( "name" );
+            String sex = Communication.enter( "sex", "1-male, 2-female, 3-both" );
+
+            try {
+                String sql = "INSERT INTO categories VALUES(DEFAULT ,\'" +
+                        name + "\', "
+                        + Communication.enter( "min_team_game" ) + ", "
+                        + Communication.enter( "max_team_game" ) + ","
+                        + Communication.enter( "min_players_team" ) + ","
+                        + Communication.enter( "max_players_team" ) + ","
+                        + Communication.enter( "id_result_type", "1-score, 2-time, 3-points" ) + ");";
+                statement.execute( sql );
+
+                String categoryID = preparer.lastUsedDefaultID( "categories" );
+                if (!Objects.equals( sex, "3" )) {
+                    sql = "INSERT INTO disciplines VALUES(DEFAULT ," + sex + "," + categoryID + ");";
+                    statement.execute( sql );
+                } else {
+                    sql = "INSERT INTO disciplines VALUES(DEFAULT ," + "1" + "," + categoryID + ");";
+                    statement.execute( sql );
+                    sql = "INSERT INTO disciplines VALUES(DEFAULT ," + "2" + "," + categoryID + ");";
+                    statement.execute( sql );
+                }
+            } catch (SQLException e) {
+                Communication.error( e );
+            }
+            System.out.println( "Success! " + name + "added.\n" );
+        }
+    }
+}
--- Pr/src/common/generators/Communication.java
+++ Pr/src/common/generators/Communication.java
@@ -1,28 +1,62 @@
 package common.generators;
 
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
+import java.util.Objects;
 
 import static common.MainApp.scanner;
 
-public class Communication {
-
-    public static void hello(String arg) {
-        System.out.printf("Adding new " + arg + ":\n");
+public class Communication{
+    public static void hello(String arg){
+        System.out.printf("Adding new "+arg+":\n");
     }
-
-    public static String enter(String word) {
-        System.out.printf("Enter " + word + ":");
+    public static String enter(String word,Boolean rand){
+        System.out.printf("Enter "+word+":(RAND to random)");
+        String s=scanner.next();
+        if (Objects.equals( s, "RAND" ))
+            return null;
+        else
+            return s;
+    }
+    public static String enter(String word){
+        System.out.printf("Enter "+word+":");
         return scanner.next();
     }
-
-    public static String enter(String word, String extraInfo) {
-        System.out.printf("Enter " + word + ":");
-        System.out.printf("(" + extraInfo + ")");
+    public static String enter(String word,String extrainfo){
+        System.out.printf("Enter "+word+":");
+        System.out.printf( "("+extrainfo+")" );
         return scanner.next();
     }
-
-    public static void error(SQLException e) {
+    public static void error(SQLException e){
         System.out.println("Something went wrong...Sorry. Try different parameters.");
-        e.printStackTrace();
+    }
+    public static void displayResultSet(ResultSet rs) {
+        try {
+            ResultSetMetaData rsmd = rs.getMetaData();
+            int columnsNumber = rsmd.getColumnCount();
+
+            for (int i = 1; i <= columnsNumber; i++) {
+
+                int ile = 15 - rsmd.getColumnName(i).length();
+                String spaces = String.format("%" + ile + "s", "");
+                System.out.print(rsmd.getColumnName(i) + spaces);
+            }
+            System.out.println();
+
+            while (rs.next()) {
+
+                for (int i = 1; i <= columnsNumber; i++) {
+
+                    int ile = 15 - rs.getString(i).length();
+                    String spaces = String.format("%" + ile + "s", "");
+                    System.out.print(rs.getString(i) + spaces);
+                }
+                System.out.println();
+            }
+        } catch (SQLException e) {
+//            TODO
+//            e.printStackTrace();
+        }
     }
 }
