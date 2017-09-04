c56b7450a603823016cd5a2699668e67629f7094
lwronski <woljako@gmail.com>
2017-09-04 03:06:04 +0200

    Add events_view

--- Pr/src/common/views.sql
+++ Pr/src/common/views.sql
@@ -1,3 +1,5 @@
+DROP VIEW IF EXISTS disciplines_views;
+DROP VIEW IF EXISTS players_views;
 DROP VIEW IF EXISTS gold_medals;
 DROP VIEW IF EXISTS silver_medals;
 DROP VIEW IF EXISTS bronze_medals;
@@ -7,7 +9,8 @@ DROP VIEW IF EXISTS players_all cascade;
 DROP VIEW IF EXISTS lol3 cascade;
 DROP VIEW IF EXISTS lol2 cascade;
 DROP VIEW IF EXISTS lol cascade;
-DROP VIEW IF EXISTS team CASCADE;
+DROP VIEW IF EXISTS teams_views CASCADE;
+DROP VIEW IF EXISTS events_views CASCADE;
 
 
 CREATE VIEW team_category AS SELECT
@@ -24,7 +27,7 @@ CREATE VIEW results AS SELECT
  join events on id_event=events.id
  join finals on id_final=finals.id;
 
-CREATE VIEW team as
+CREATE VIEW teams_views as
  select teams.id ,sexes.name as sex,categories.name as category,nationalities.name as nationality
  from teams join sexes on teams.id_sex=sexes.id
  join disciplines on id_discipline=disciplines.id
@@ -54,3 +57,14 @@ CREATE VIEW silver_medals AS SELECT country,lol2.category
 CREATE VIEW bronze_medals AS SELECT country,lol3.category
  from results join lol3 on results.final=lol3.final and results.category=lol3.category
        where results.result=lol3.scoremax;
+
+CREATE VIEW disciplines_views AS SELECT d.id, sexes.name as sex_name, c.name as name_category from disciplines as d join sexes on sexes.id = d.id_sex join categories as c on c.id = d.id_categories;
+
+CREATE VIEW players_views AS SELECT p.id, p.first_name, p.last_name, n.name, p.birth_date, s.name as sex_name from players as p 
+	join nationalities as n on p.id_nationality = n.id
+	join sexes as s on s.id = p.id_sex;
+
+CREATE VIEW events_views AS SELECT e.id, p.name as place_name, e.start_time, e.end_time, d.sex_name, d.name_category, f.name as name_finals from events as e 
+	join places as p on e.id_place = p.id
+	join disciplines_views as d on d.id = e.id_disciplines
+	join finals as f on f.id = e.id_final;
